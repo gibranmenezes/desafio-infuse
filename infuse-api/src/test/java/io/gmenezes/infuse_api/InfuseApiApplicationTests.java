8836342617fb061a9afa691119232029d2d44f08
gibran <gibranquimica@gmail.com>
2025-05-31 08:07:54 -0300

    feat: initial commit

---
+++ infuse-api/src/test/java/io/gmenezes/infuse_api/InfuseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gmenezes.infuse_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfuseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
