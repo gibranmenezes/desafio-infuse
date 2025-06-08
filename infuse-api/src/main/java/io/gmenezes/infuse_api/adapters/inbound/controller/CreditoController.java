c7ac1f203484fc167d1776f89c1ae54f9d2bad12
gibran <gibranquimica@gmail.com>
2025-06-08 10:03:21 -0300

    refact: implementacao de endpoint.

--- infuse-api/src/main/java/io/gmenezes/infuse_api/adapters/inbound/controller/CreditoController.java
+++ infuse-api/src/main/java/io/gmenezes/infuse_api/adapters/inbound/controller/CreditoController.java
@@ -1,23 +1,30 @@
 package io.gmenezes.infuse_api.adapters.inbound.controller;
 
+import io.gmenezes.infuse_api.application.services.CreditoUseCasesImpl;
 import io.gmenezes.infuse_api.domain.credito.dtos.CreditoResponse;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController (" /api/creditos/")
+@RestController ("/api/creditos/")
 @RequiredArgsConstructor
 public class CreditoController {
 
+    private final CreditoUseCasesImpl useCases;
+
 
     @GetMapping("/{numeroNfse}")
-    public List<CreditoResponse> buscarPorNfse(@NotBlank @PathVariable String numeroNfse) {
+    public ResponseEntity<List<CreditoResponse>> buscarPorNfse(@NotBlank @PathVariable String numeroNfse) {
+        List<CreditoResponse> creditos = useCases.getCreditosByNfse(numeroNfse);
 
-        return null;
+        if (creditos.isEmpty()) return ResponseEntity.noContent().build();
+
+        return ResponseEntity.ok(creditos);
     }
 
 
