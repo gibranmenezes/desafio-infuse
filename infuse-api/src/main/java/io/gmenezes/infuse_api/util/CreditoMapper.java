9d71e27687ef42656194db57a5a600764349e74f
gibranmenezes <gibran.menezes@seventh.com.br>
2025-06-12 08:57:03 -0300

    refact: ajustando metodos de mapeamento.

--- infuse-api/src/main/java/io/gmenezes/infuse_api/util/CreditoMapper.java
+++ infuse-api/src/main/java/io/gmenezes/infuse_api/util/CreditoMapper.java
@@ -1,14 +1,20 @@
 package io.gmenezes.infuse_api.util;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import io.gmenezes.infuse_api.adapters.outbound.entities.JpaCreditoEntity;
 import io.gmenezes.infuse_api.domain.credito.Credito;
 import io.gmenezes.infuse_api.domain.credito.dtos.CreditoResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface CreditoMapper {
 
-    ObjectMapper mapper = new ObjectMapper();
+    @Mapping(target = "dataConstituicao", expression = "credito.getDataConstituicao().format(java.time.format.DateTimeFormatter.ofPattern(\"yyyy-MM-dd\"))")
+    @Mapping(target = "simplesNacional", expression = "java(credito.isSimplesNacional() ? \"Sim\" : \"Não\")")
+    CreditoResponse fromCreditoToResponse(Credito credito);
 
-    static CreditoResponse fromCreditoToResponse (Credito credito) {
-        return mapper.convertValue(credito, CreditoResponse.class);
-    }
+
+    Credito fromJpaEntityToCredito(JpaCreditoEntity jpaCreditoEntity);
+
 }
