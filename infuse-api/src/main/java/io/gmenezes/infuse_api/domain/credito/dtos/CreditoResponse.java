5f9e65b3fed6c26bbc36d232f3cf6b97a28c48f0
gibranmenezes <gibran.menezes@seventh.com.br>
2025-06-12 08:57:25 -0300

    refact: alterando campo boolean para string

--- infuse-api/src/main/java/io/gmenezes/infuse_api/domain/credito/dtos/CreditoResponse.java
+++ infuse-api/src/main/java/io/gmenezes/infuse_api/domain/credito/dtos/CreditoResponse.java
@@ -1,25 +1,17 @@
 package io.gmenezes.infuse_api.domain.credito.dtos;
 
-import io.gmenezes.infuse_api.domain.credito.Credito;
-
 import java.math.BigDecimal;
-import java.time.format.DateTimeFormatter;
 
 public record CreditoResponse(String numeroCredito,
                               String numeroNfse,
                               String dataConstituicao,
                               BigDecimal valorIssqn,
                               String tipoCredito,
-                              boolean simplesNacional,
+                              String simplesNacional,
                               BigDecimal  aliquota,
                               BigDecimal  valorFaturado,
                               BigDecimal  valorDeducao,
                               BigDecimal  baseCalculo) {
 
-    public CreditoResponse(Credito credito) {
-        this(credito.getNumeroCredito(), credito.getNumeroNfse(),
-                credito.getDataConstituicao().format(DateTimeFormatter.ISO_DATE),
-                credito.getValorIssqn(), credito.getTipoCredito(), credito.isSimplesNacional(),
-                credito.getAliquota(), credito.getValorFaturado(), credito.getValorDeducao(), credito.getBaseCalculo());
-    }
+
 }
