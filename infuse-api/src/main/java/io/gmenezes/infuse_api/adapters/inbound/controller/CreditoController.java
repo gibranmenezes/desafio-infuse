0ae5d3b3ede7c03a22183bdded4c5d8713fab5ea
gibranmenezes <gibran.menezes@seventh.com.br>
2025-06-13 13:48:45 -0300

    refact: ajustando path do controller

--- infuse-api/src/main/java/io/gmenezes/infuse_api/adapters/inbound/controller/CreditoController.java
+++ infuse-api/src/main/java/io/gmenezes/infuse_api/adapters/inbound/controller/CreditoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/creditos")
+@RequestMapping("/api/creditos")
 @RequiredArgsConstructor
 public class CreditoController {
 
