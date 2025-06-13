a7432f6729324c0b9f676a9aa8cb27f6f73fc03e
gibranmenezes <gibran.menezes@seventh.com.br>
2025-06-13 13:49:13 -0300

    feat: configuracao de cors

--- infuse-api/src/main/java/io/gmenezes/infuse_api/infrastructure/configs/CorsConfig.java
+++ infuse-api/src/main/java/io/gmenezes/infuse_api/infrastructure/configs/CorsConfig.java
@@ -1,4 +1,17 @@
 package io.gmenezes.infuse_api.infrastructure.configs;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
