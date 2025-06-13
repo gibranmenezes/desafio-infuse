28807b85f0cd82a0dd387d40d055e1a9786e8a8a
gibranmenezes <gibran.menezes@seventh.com.br>
2025-06-13 09:31:28 -0300

    refact: configuração de testes

--- infuse-api/src/test/java/io/gmenezes/infuse_api/InfuseApiApplicationTests.java
+++ infuse-api/src/test/java/io/gmenezes/infuse_api/InfuseApiApplicationTests.java
@@ -2,8 +2,10 @@ package io.gmenezes.infuse_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class InfuseApiApplicationTests {
 
 	@Test
