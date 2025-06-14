b9114dc00a3d5d0699940add2c54afbcaf8f64d1
gibranmenezes <gibran.menezes@seventh.com.br>
2025-06-14 15:30:44 -0300

    refact: implementando mapper e adicionando cors

--- infuse-api/src/main/java/io/gmenezes/infuse_api/infrastructure/configs/CorsConfig.java
+++ infuse-api/src/main/java/io/gmenezes/infuse_api/infrastructure/configs/CorsConfig.java
@@ -4,14 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import static org.springframework.http.CacheControl.maxAge;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "http://frontend:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
--- infuse-api/src/main/java/io/gmenezes/infuse_api/util/CreditoMapper.java
+++ infuse-api/src/main/java/io/gmenezes/infuse_api/util/CreditoMapper.java
@@ -22,6 +22,16 @@ public interface CreditoMapper {
     @Mapping(target = "baseCalculo")
     CreditoResponse fromCreditoToResponse(Credito credito);
 
+    @Mapping(target = "numeroCredito", source = "numeroCredito")
+    @Mapping(target = "numeroNfse", source = "numeroNfse")
+    @Mapping(target = "dataConstituicao", source = "dataConstituicao")
+    @Mapping(target = "valorIssqn", source = "valorIssqn")
+    @Mapping(target = "tipoCredito", source = "tipoCredito")
+    @Mapping(target = "simplesNacional", source = "simplesNacional")
+    @Mapping(target = "aliquota", source = "aliquota")
+    @Mapping(target = "valorFaturado", source = "valorFaturado")
+    @Mapping(target = "valorDeducao", source = "valorDeducao")
+    @Mapping(target = "baseCalculo", source = "baseCalculo")
     Credito fromJpaEntityToCredito(JpaCreditoEntity jpaCreditoEntity);
 
 }
